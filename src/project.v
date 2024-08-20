fec46147928975331ab65ecf393ff4f0f06fca33
bastian-mohr <88044697+bastian-mohr@users.noreply.github.com>
2024-08-20 21:17:48 +0200

    Update project.v
    small counter implementation

--- src/project.v
+++ src/project.v
@@ -16,12 +16,20 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // All output pins must be assigned. If not used, assign to 0.  
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+  reg [7:0] my_counter;
+
+    always @(posedge clk) begin
+        if (!rst_n)
+          my_counter = 0;
+        else
+          my_counter <= my_counter + 1;
+    end
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  assign uo_out = my_counter;
 
 endmodule
